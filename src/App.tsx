c74993e73880ebfe5dfbd514539d62e4d85e73ea
HoangTB <tbhoang95@gmail.com>
2024-05-24 16:52:20 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
 import React from 'react';
+import '@fontsource/montserrat'; // Defaults to weight 400
+import '@fontsource/montserrat/400.css'; // Specify weight
+import '@fontsource/montserrat/400-italic.css'; // Specify weight and style
 
 function App() {
   return <RouterProvider router={router} fallbackElement={<p>ok nLoading on test....</p>} />;
