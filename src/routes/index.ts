1773c905b2de4127e248a8afe7532d36bbbbdfd6
buivansy <sy.bui@bkitsolution.com>
2024-05-27 16:36:32 +0700

    feat: page ui contact about us

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -13,6 +13,8 @@ import Payment from '../pages/payment/Payment';
 import BookedHistory from '../pages/booked-history/BookedHistory';
 import Completed from '../pages/payment-completed/Completed';
 import VoteHistory from '../pages/vote-history/VoteHistory';
+import Contact from '../pages/contact/Contact';
+import AboutUs from '../pages/about-us/AboutUs';
 
 const routes: RouteObject[] = [
   {
@@ -62,6 +64,14 @@ const routes: RouteObject[] = [
       {
         path: '/booking/completed',
         Component: Completed,
+      },
+      {
+        path: '/contact',
+        Component: Contact,
+      },
+      {
+        path: 'about-us',
+        Component: AboutUs
       },
     ],
   },
