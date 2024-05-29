d77041d4e29e6ca7d6f26756be269b127d5c2922
DiemTTT <thuydiem17022001@gmail.com>
2024-05-29 10:17:33 +0700

    feat: login

--- src/pages/login/login-page.tsx
+++ src/pages/login/login-page.tsx
@@ -4,6 +4,8 @@ import React, { useState } from 'react';
 import CustomButton from '../../components/buttons/submit-button/custom-button';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { ILogin } from '../../types/user';
+import { login } from '../../api/user/user-api';
 
 interface LoginFormValues {
   email: string;
@@ -18,24 +20,27 @@ const LoginPage = () => {
   const onFinish = async (values: LoginFormValues) => {
     setLoading(true);
     setError('');
-    try {
-      const response = await axios.post('http://localhost:8081/api/auth/login', {
-        email: values.email,
-        password: values.password,
-      });
+      setLoading(true);
+      try {
+        const token = await login(values as ILogin)
+        if(token) {
+          localStorage.setItem('token', token.accessToken);
+          localStorage.setItem('roleList', JSON.stringify(token.roleList));
 
-      // Assuming the response contains the token
-      const { accessToken, roleList } = response.data;
-      // Store the token in localStorage or a context/state management solution
-      localStorage.setItem('accessToken', accessToken);
-      localStorage.setItem('roleList', JSON.stringify(roleList));
+        }
 
-      // Redirect to a protected route, e.g., dashboard
-      navigate('/home');
-    } catch (error) {
-      setError('Login failed. Please check your email and password.');
-      setLoading(false);
-    }
+        if(JSON.stringify(token.roleList).includes("ROLE_ADMIN")){
+          navigate("/admin")
+        }else{
+          navigate("/")
+        }
+      } catch (error) {
+        setError('Login failed. Please check your email and password.');
+        // localStorage.setItem('roleList', JSON.stringify(roleList));
+      } finally {
+        setLoading(false);
+
+      }
   };
 
   const onFinishFailed = (errorInfo: any) => {
