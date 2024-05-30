3534a1bca62ae57db5bf5fe9bbabbfdb79837ba3
LamNT80 <truonglam83.dev@gmail.com>
2024-05-30 16:28:16 +0700

    fix: fix merge code

--- src/pages/login/login-page.tsx
+++ src/pages/login/login-page.tsx
@@ -20,27 +20,25 @@ const LoginPage = () => {
   const onFinish = async (values: LoginFormValues) => {
     setLoading(true);
     setError('');
-      setLoading(true);
-      try {
-        const token = await login(values as ILogin)
-        if(token) {
-          localStorage.setItem('token', token.accessToken);
-          localStorage.setItem('roleList', JSON.stringify(token.roleList));
-
-        }
-
-        if(JSON.stringify(token.roleList).includes("ROLE_ADMIN")){
-          navigate("/admin")
-        }else{
-          navigate("/")
-        }
-      } catch (error) {
-        setError('Login failed. Please check your email and password.');
-        // localStorage.setItem('roleList', JSON.stringify(roleList));
-      } finally {
-        setLoading(false);
-
+    setLoading(true);
+    try {
+      const token = await login(values as ILogin);
+      if (token) {
+        localStorage.setItem('token', token.accessToken);
+        localStorage.setItem('roleList', JSON.stringify(token.roleList));
       }
+
+      if (JSON.stringify(token.roleList).includes('ROLE_ADMIN')) {
+        navigate('/admin');
+      } else {
+        navigate('/');
+      }
+    } catch (error) {
+      setError('Login failed. Please check your email and password.');
+      // localStorage.setItem('roleList', JSON.stringify(roleList));
+    } finally {
+      setLoading(false);
+    }
   };
 
   const onFinishFailed = (errorInfo: any) => {
@@ -84,7 +82,7 @@ const LoginPage = () => {
           </Form.Item>
           <Form.Item className={styles.customBtn}>
             <CustomButton type="primary" htmlType="submit" loading={loading}>
-              Log in
+              {loading ? 'Logging in...' : 'Log in'}
             </CustomButton>
           </Form.Item>
         </Form>
